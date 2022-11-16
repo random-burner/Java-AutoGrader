5db0dd3f2dd84c97793d927bd059e7d83a0844e0
EastArctica <118198186+random-burner@users.noreply.github.com>
2022-11-15 23:45:39 -0500

    Port main.py to index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+const autograder = require('./autograder/');
+const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const iterator = rl[Symbol.asyncIterator]();
+
+let projectName = '';
+let testPath = '';
+let projectPath = '';
+
+!(async () => {
+    process.stdout.write('Do you want to download projects (y/n): ');
+    let shouldDownloadProjects = (await iterator.next()).value.trim().toLowerCase();
+    if (shouldDownloadProjects == 'y' || shouldDownloadProjects == 'yes') {
+        process.stdout.write('Specify Project Name (name of test file): ');
+        projectName = (await iterator.next()).value;
+
+        testPath = process.cwd() + `../tests/${projectName}.json`;
+        projectPath = process.cwd() + `../projects/${projectName}.json`;
+    
+        if (!fs.existsSync(testPath)) {
+            process.stdout.write(`There are no tests that match name ${projectName}. Continue?`);
+            let shouldContinue = (await iterator.next()).value;
+            if (shouldContinue != 'yes' && shouldContinue != 'y' && shouldContinue != '') {
+                process.exit(0);
+            }
+        }
+
+        process.stdout.write('Do you want to delete old projects: ');
+        let shouldDeleteExisting = (await iterator.next()).value.toLowerCase();
+        shouldDeleteExisting = shouldDeleteExisting == 'yes' || shouldDeleteExisting == 'y';
+    
+        autograder.cleanup_folder({ deleteExisting: shouldDeleteExisting, projectsDir: projectPath });
+    
+        process.stdout.write('List Projects separated by spaces: ');
+        let projects = (await iterator.next()).value.trim().split(' ');
+        autograder.download_projects({ projects, downloadDir: projectPath });
+        autograder.compile_projects({ projectsDir: projectPath });
+    }
+
+    process.stdout.write('Do you want to test projects (y/n): ');
+    let shouldTestProjects = (await iterator.next()).value.trim().toLowerCase();
+    if (shouldTestProjects == 'y' || shouldTestProjects == 'yes') {
+        if (projectName == '') {
+            process.stdout.write('Specify Project Name (name of test file): ');
+            projectName = (await iterator.next()).value;
+            testPath = process.cwd() + `../tests/${projectName}.json`;
+            projectPath = process.cwd() + `../projects/${projectName}.json`;
+        }
+
+        if (!fs.existsSync(testPath)) {
+            process.stdout.write(`There are no tests that match name ${projectName}.`);
+            process.exit(0);
+        }
+
+        let res = autograder.testProjects({ projectPath, testPath });
+        let outputStr = `Project \`${projectName}\` passed ${res.passedTests.reduce((a, b) => a + b, 0)}/${totalTests}`;
+        for (const result of res.tests) {
+            outputStr += result.success ? '\x1b[34m✓' : '\x1b[31mX';
+        }
+    }
+})();
